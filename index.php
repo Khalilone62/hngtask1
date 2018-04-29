8e71add3956bba138039c2a47186390ba0004fa8
Ifeoluwa Olawoye <ifeoluwa.olawoye@gmail.com>
2018-04-29 16:06:51 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home | HNG Internship 4</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="main">
+		<h3 class="welcome-header-text">Welcome To</h3>
+		<h1 class="hng-header-text">HNG Internship <br>4</h1>
+		<?php 
+			$date_text = date("D, d M Y h:i A");
+			echo '<p class="date-text">'.$date_text.'</p>';
+		?>
+		
+	</div>
+</body>
+</html>
