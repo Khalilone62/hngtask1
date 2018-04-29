bcc7dfd480f2da275faba5da0e50d72bf937cbe3
Olaoluwani Onafowope <olaoluwanionafowope@gmail.com>
2018-04-29 16:53:43 +0100

    initial commit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
 	<div class="main">
 		<h3 class="welcome-header-text">Welcome To</h3>
-		<h1 class="hng-header-text">HNG Internship <br>4</h1>
+		<h1 class="hng-name-header-text">HNG Internship <br>4</h1>
 		<?php 
 			$date_text = date("D, d M Y h:i A");
 			echo '<p class="date-text">'.$date_text.'</p>';
